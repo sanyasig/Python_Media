577fd898ec39139a7675ce8f394bb5b602b7072b
nani <nani76.gsrao@gmail.com>
2018-03-18 20:03:31 +0000

    first commit

---
+++ config_parser.py
@@ -0,0 +1,22 @@
+import configparser
+import  os
+
+config = 0;
+
+def read_config():
+    config = configparser.ConfigParser()
+    config.read(os.path.expanduser('~') + '/work/home_settings.ini')
+    print(config.sections())
+    return config
+
+
+def get_config(type):
+    config = read_config()
+    return {
+        'youtube': config['youtube'],
+        'b': 2,
+        'pc' : config['pc'],
+    }[type]
+
+
+
